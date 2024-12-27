3566f0c8d2919174fbc596ead529b3d004e78640
Big Bishop David <thebigdavid360@outlook.com>
2024-12-27 22:11:30 +0100

    Updated homepage to display username if user is logged in and login or register button if user is logged out and also updated a bug in the logout file

--- auth/logout.php
+++ auth/logout.php
@@ -3,5 +3,5 @@
     session_unset();
     session_destroy();
     
-    header("Location: http:localhost/homeland/");
+    header("Location: http://localhost/homeland/");
 ?>
--- index.php
+++ index.php
@@ -42,18 +42,22 @@
                     </ul>
                   </li>
 
-                  <li class="has-children">
-                    <a href="properties.html"><?php echo $_SESSION['user_name'];?></a>
-                    <ul class="dropdown arrow-top">
-                      <li><a href="<?php echo APPURL?>auth/logout.php">Logout</a></li>
-                     
-                    </ul>
-                  </li>
-
                   <li><a href="about.html">About</a></li>
                   <li><a href="contact.html">Contact</a></li>
-                  <li><a href="login.html">Login</a></li>
-                  <li><a href="register.html">Register</a></li>
+
+                  <?php if (isset($_SESSION["user_name"])) : ?>
+                    <li class="has-children">
+                      <a href="properties.html"><?php echo $_SESSION['user_name'];?></a>
+                      <ul class="dropdown arrow-top">
+                        <li><a href="<?php echo APPURL?>auth/logout.php">Logout</a></li>
+                      
+                      </ul>
+                  </li>
+
+                  <?php else : ?>
+                    <li><a href="<?php echo APPURL?>auth/login.php">Login</a></li>
+                    <li><a href="<?php echo APPURL?>auth/register.php">Register</a></li>
+                  <?php endif;?>
                 </ul>
               </nav>
             </div>
