7ea35b9eb949ef4af8b88954cb7041ab5137e91d
Big Bishop David <thebigdavid360@outlook.com>
2024-12-27 22:22:28 +0100

    Added the navbar code in header.php for ease of editing since its the same in all files

--- includes/header.php
+++ includes/header.php
@@ -30,3 +30,56 @@
     <link rel="stylesheet" href="<?php echo APPURL;?>css/style.css">
     
   </head>
+
+  <div class="site-navbar mt-4">
+        <div class="container py-1">
+          <div class="row align-items-center">
+            <div class="col-8 col-md-8 col-lg-4">
+              <h1 class="mb-0"><a href="index.php" class="text-white h2 mb-0"><strong>Homeland<span class="text-danger">.</span></strong></a></h1>
+            </div>
+            <div class="col-4 col-md-4 col-lg-8">
+              <nav class="site-navigation text-right text-md-right" role="navigation">
+
+                <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+                <ul class="site-menu js-clone-nav d-none d-lg-block">
+                  <li class="active">
+                    <a href="index.php">Home</a>
+                  </li>
+                  <li><a href="buy.html">Buy</a></li>
+                  <li><a href="rent.html">Rent</a></li>
+                  <li class="has-children">
+                    <a href="properties.html">Properties</a>
+                    <ul class="dropdown arrow-top">
+                      <li><a href="#">Condo</a></li>
+                      <li><a href="#">Property Land</a></li>
+                      <li><a href="#">Commercial Building</a></li>
+                     
+                    </ul>
+                  </li>
+
+                  <li><a href="about.html">About</a></li>
+                  <li><a href="<?php echo APPURL?>contact.php">Contact</a></li>
+
+                  <?php if (isset($_SESSION["user_name"])) : ?>
+                    <li class="has-children">
+                      <a href="properties.html"><?php echo $_SESSION['user_name'];?></a>
+                      <ul class="dropdown arrow-top">
+                        <li><a href="<?php echo APPURL?>auth/logout.php">Logout</a></li>
+                      
+                      </ul>
+                  </li>
+
+                  <?php else : ?>
+                    <li><a href="<?php echo APPURL?>auth/login.php">Login</a></li>
+                    <li><a href="<?php echo APPURL?>auth/register.php">Register</a></li>
+                  <?php endif;?>
+                </ul>
+              </nav>
+            </div>
+           
+
+          </div>
+        </div>
+      </div>
+    </div>
