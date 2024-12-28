286d307f923ecb74a7d9fc6ad65610262c8ddf3b
Big Bishop David <thebigdavid360@outlook.com>
2024-12-28 03:28:32 +0100

    Updated the homepage to display dynamic banner from the database and a few minor other changes

--- index.php
+++ index.php
@@ -1,33 +1,31 @@
-<?php require_once "includes/header.php"?>
+<?php 
+  require_once "includes/header.php";
+  require_once "config/config.php";
+?>
 
+<?php
+
+  $query = $conn->query("SELECT * FROM props");
+  $query->execute();
+
+  $props = $query->fetchAll(PDO::FETCH_OBJ);
+
+?>
     <div class="slide-one-item home-slider owl-carousel">
-
-      <div class="site-blocks-cover overlay" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-        <div class="container">
-          <div class="row align-items-center justify-content-center text-center">
-            <div class="col-md-10">
-              <span class="d-inline-block bg-success text-white px-3 mb-3 property-offer-type rounded">For Rent</span>
-              <h1 class="mb-2">871 Crenshaw Blvd</h1>
-              <p class="mb-5"><strong class="h2 text-success font-weight-bold">$2,250,500</strong></p>
-              <p><a href="#" class="btn btn-white btn-outline-white py-3 px-5 rounded-0 btn-2">See Details</a></p>
+      <?php foreach($props as $prop): ?>
+        <div class="site-blocks-cover overlay" style="background-image: url(images/<?php echo $prop->image; ?>);" data-aos="fade" data-stellar-background-ratio="0.5">
+          <div class="container">
+            <div class="row align-items-center justify-content-center text-center">
+              <div class="col-md-10">
+                <span class="d-inline-block bg-success text-white px-3 mb-3 property-offer-type rounded"><?php echo $prop->type; ?></span>
+                <h1 class="mb-2"><?php echo $prop->name; ?></h1>
+                <p class="mb-5"><strong class="h2 text-success font-weight-bold">$<?php echo $prop->price; ?></strong></p>
+                <p><a href="<?php echo APPURL?>property-details.php?id=<?php echo $prop->id; ?>" class="btn btn-white btn-outline-white py-3 px-5 rounded-0 btn-2">See Details</a></p>
+              </div>
             </div>
           </div>
-        </div>
-      </div>  
-
-      <div class="site-blocks-cover overlay" style="background-image: url(images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-        <div class="container">
-          <div class="row align-items-center justify-content-center text-center">
-            <div class="col-md-10">
-              <span class="d-inline-block bg-danger text-white px-3 mb-3 property-offer-type rounded">For Sale</span>
-              <h1 class="mb-2">625 S. Berendo St</h1>
-              <p class="mb-5"><strong class="h2 text-success font-weight-bold">$1,000,500</strong></p>
-              <p><a href="#" class="btn btn-white btn-outline-white py-3 px-5 rounded-0 btn-2">See Details</a></p>
-            </div>
-          </div>
-        </div>
-      </div>  
-
+        </div>  
+      <?php endforeach ?>
     </div>
 
 
