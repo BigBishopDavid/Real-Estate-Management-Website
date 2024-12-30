4cee1cca6ff6748f69f201809298a54447b81229
Big Bishop David <thebigdavid360@outlook.com>
2024-12-30 09:43:21 +0100

    Made a few changes

--- search.php
+++ search.php
@@ -91,39 +91,7 @@
               </div>
             </div>
           </form>
-        </div>  
-
-        <div class="row">
-          <div class="col-md-12">
-            <div class="view-options bg-white py-3 px-3 d-md-flex align-items-center">
-              <div class="mr-auto">
-                <a href="index.html" class="icon-view view-module active"><span class="icon-view_module"></span></a>
-                <a href="view-list.html" class="icon-view view-list"><span class="icon-view_list"></span></a>
-                
-              </div>
-              <div class="ml-auto d-flex align-items-center">
-                <div>
-                  <a href="#" class="view-list px-3 border-right active">All</a>
-                  <a href="#" class="view-list px-3 border-right">Rent</a>
-                  <a href="#" class="view-list px-3">Sale</a>
-                </div>
-
-
-                <div class="select-wrap">
-                  <span class="icon icon-arrow_drop_down"></span>
-                  <select class="form-control form-control-sm d-block rounded-0">
-                    <option value="">Sort by</option>
-                    <option value="">Price Ascending</option>
-                    <option value="">Price Descending</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-       
-      </div>
-    </div>
 
     <div class="site-section site-section-sm bg-light">
       <div class="container">
@@ -165,10 +133,10 @@
                 </div>
             </div>
             <?php endforeach?>
+        </div>
             <?php else : ?>
                 <div class="bg-success text-white">We do not have a listing with this query for now</div>
 
-        </div>
         <?php endif?>
      
         
